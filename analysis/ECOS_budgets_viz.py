d46dcb00c17c70a87db4d52ab2d9a41dc6953b4f
Nathan Sanders <sandersn@gmail.com>
2017-07-23 17:22:06 -0400

    Added ECOS budget per capita plot

--- analysis/ECOS_budgets_viz.py
+++ analysis/ECOS_budgets_viz.py
@@ -36,6 +36,9 @@ ECOS_years = np.unique(safe_cast(s_data.Year.values)).astype(str)
 f_data = pd.read_sql_query('SELECT * FROM MassBudget_summary', disk_engine)
 f_data.index = f_data.Year
 
+## Get Census population data
+statepop_data = pd.read_sql_query('SELECT * FROM Census_statepop', disk_engine)
+
 ## Get DEP funding data
 inf_data = pd.read_sql_query('SELECT * FROM SSAWages', disk_engine)
 inf_data.index = inf_data.Year.astype(str)
@@ -85,6 +88,22 @@ mychart.jekyll_write('../docs/_includes/charts/ECOS_budget_peryear_bystate.html'
 ## Show per capita budget per year by state
 #############################
 
+## Establish chart
+mychart = chartjs.chart("ECOS Budget per capita by State Per Year", "Line", 640, 480)
+mychart.set_labels(ECOS_years)
+for i,state in enumerate(states):
+	statepop_data_sel = statepop_data[statepop_data['State']==state].T.ix[ECOS_years]
+	vals = s_data_g.get_group(state).set_index('Year').ix[ECOS_years].value.astype(float) * (inf_data_sel['correct'] / statepop_data_sel.T) / 1e3 
+	mychart.add_dataset(
+		vals.astype(float).values[0], 
+		state,
+		backgroundColor="'"+(color_cycle*10)[i]+"'",
+		stack="'annual'", yAxisID= "'y-axis-0'", fill = "false",
+		hidden = 'false' if state in ['Massachusetts','New Hampshire','Vermont','Maine','Rhode Island'] else 'true')
+mychart.set_params(JSinline = 0, ylabel = 'Reported Environmental Agency Budget (ECOS, $k per capita)', xlabel='Year',
+	scaleBeginAtZero=1)
+
+mychart.jekyll_write('../docs/_includes/charts/ECOS_budget_percap_peryear_bystate.html')
 
 
 
