bb2e683ad37b83882214b1e2ba8bafbeaee15e1f
Nathan Sanders <sandersn@gmail.com>
2017-08-13 19:31:13 -0400

    Reverted polygon-level popup work

--- analysis/EEADP_inspection_map.py
+++ analysis/EEADP_inspection_map.py
@@ -40,18 +40,6 @@ map_1 = folium.Map(
 	tiles='Stamen Terrain',
 	)
 
-## Add popup information as illustrated here https://nbviewer.jupyter.org/github/ocefpaf/folium_notebooks/blob/618e31754aa9325cae6c315b687bea95f3f92ae4/test_geojson_popup.ipynb
-## See example geojson file associated with it, here: https://raw.githubusercontent.com/ocefpaf/folium_notebooks/618e31754aa9325cae6c315b687bea95f3f92ae4/data/tri.json
-for i in range(len(geo_towns_dict)):
-	town = geo_towns_dict[i]['properties']['TOWN']
-	if town in data_ins_g_t:
-		## Gather town data
-		inspections = data_ins_g_t.ix[town]
-		pop10 = geo_towns_dict[i]['properties']['POP2010']
-		## Add marker
-		popup_text = town+'<br>Population (2010): '+str(pop10)+'<br>Total inspections: '+str(inspections)
-		geo_towns_dict[i]['properties']['popupContent'] = popup_text
-		
 ## Draw choropleth
 map_1.choropleth(
 	geo_path=geo_towns, 
@@ -62,26 +50,6 @@ map_1.choropleth(
 	threshold_scale = list(np.percentile(data_ins_g_t, [0,50,90,100])),
 	fill_color='PuBu', fill_opacity=0.7, line_opacity=0.3, highlight=True,
 	)
-### Add markers
-#for i in range(len(geo_towns_dict)):
-	#town = geo_towns_dict[i]['properties']['TOWN']
-	#if town in data_ins_g_t:
-		### Gather town data
-		#inspections = data_ins_g_t.ix[town]
-		#pop10 = geo_towns_dict[i]['properties']['POP2010']
-		### Take average position of polygon points for marker center
-		#raw_coords = geo_towns_dict[i]['geometry']['coordinates']
-		#if np.ndim(raw_coords) == 2: # Take most complicated polygon if multiple are provided
-			#j = np.argmax([np.shape(raw_coords[i][0])[0] for i in range(len(raw_coords))])
-			#coords = raw_coords[j]
-		#elif np.ndim(raw_coords) == 3:
-			#coords = raw_coords
-		#else: 
-			#raise
-		#mean_pos = list(np.mean(coords, axis=1)[0])[::-1]
-		### Add marker
-		#popup_text = town+'\nPopulation (2010): '+str(pop10)+'\nTotal inspections: '+str(inspections)
-		#folium.Marker(mean_pos, popup=popup_text).add_to(map_1)
 ## Save to html
 map_1.save(out_path+'EEADP_ins_map_total.html')
 
